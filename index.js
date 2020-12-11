270db03dd63ad94ae2d5dba57f2f1dbf345f8c0d
Alex Simonok <sashasimonok@gmail.com>
2020-12-11 20:33:24 +0300

    Fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const getSafeProperty = require('./utils')
 
-const getName = getSafeProperty(['item', 'name'])
+const getName = getSafeProperty(['item', 'item', 'name'])
 
 module.exports = {
   getName,
